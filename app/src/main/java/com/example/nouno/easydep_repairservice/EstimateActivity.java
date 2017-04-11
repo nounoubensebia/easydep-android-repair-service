c24c179f7496b44c9d7c66822747eab180f0ceac
nouno <nounoubensebia96@gmail.com>
2017-04-11 08:35:25 +0100

    fixed wrappers

---
+++ app/src/main/java/com/example/nouno/easydep_repairservice/EstimateActivity.java
@@ -0,0 +1,13 @@
+package com.example.nouno.easydep_repairservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EstimateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estimate);
+    }
+}
